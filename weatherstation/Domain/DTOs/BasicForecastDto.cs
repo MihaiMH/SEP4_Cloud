38fd781e342dd5d38ca4ed808be5e020fe45a76a
Stefan Buzu <stefanbuzu@gmail.com>
2024-05-08 18:17:37 +0200

    Added DTOs

---
+++ weatherstation/Domain/DTOs/BasicForecastDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.DTOs;
+
+public class BasicForecastDto
+{   
+    public DateTime Time { get; }
+    public double Temperature { get; }
+    public string WeatherState { get; }
+
+    public BasicForecastDto(DateTime time, double temperature, string weatherForecast)
+    {
+        Time = time;
+        Temperature = temperature;
+        WeatherState = weatherForecast;
+    }
+}
--- weatherstation/Domain/Models/Weather.cs
+++ weatherstation/Domain/Models/Weather.cs
@@ -7,15 +7,17 @@ public class Weather
     public double Temperature { get; set; }
     public string Light { get; set; }
     public DateTime DateTime { get; set; }
+    public string WeatherState { get; set; }
     
     public Weather() {}
     
-    public Weather(int id, double humidity, double temperature, string light, DateTime dateTime)
+    public Weather(int id, double humidity, double temperature, string light, DateTime dateTime, string weatherState)
     {
         Id = id;
         Humidity = humidity;
         Temperature = temperature;
         Light = light;
         DateTime = dateTime;
+        WeatherState = weatherState;
     }
 }
