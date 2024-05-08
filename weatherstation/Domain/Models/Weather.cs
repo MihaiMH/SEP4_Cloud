8c2689a587fc2f6863209588c763e301bb40d55c
Cristian Josan <josan.c2003@gmail.com>
2024-05-08 13:12:48 +0200

    Add domain with models, EfcDataAcces and IoTCommunication.

---
+++ weatherstation/Domain/Models/Weather.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain.Models;
+
+public class Weather
+{
+    public int Id { get; set; }
+    public double Humidity { get; set; }
+    public double Temperature { get; set; }
+    public string Light { get; set; }
+    public DateTime DateTime { get; set; }
+    
+    public Weather() {}
+    
+    public Weather(int id, double humidity, double temperature, string light, DateTime dateTime)
+    {
+        Id = id;
+        Humidity = humidity;
+        Temperature = temperature;
+        Light = light;
+        DateTime = dateTime;
+    }
+}
