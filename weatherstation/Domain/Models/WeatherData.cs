f197bd38d5392557da8a98fc5e7d3e8e4f7e76c2
Stefan Buzu <stefanbuzu@gmail.com>
2024-05-09 13:34:54 +0200

    Added the interfaces and the models

---
+++ weatherstation/Domain/Models/WeatherData.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class WeatherData
+{
+	public string WeatherState { get; set; }
+	public double Temperature { get; set; }
+	public string Light { get; set; }
+	public double Humidity { get; set; }
+	public DateTime DateTime { get; set; }
+
+	public WeatherData() { }
+
+	public WeatherData(string weatherState, double temperature, string light, double humidity, DateTime dateTime) {
+		WeatherState = weatherState;
+		Temperature = temperature;
+		Light = light;
+		Humidity = humidity;
+		DateTime = dateTime;
+	}
+
+}
