10edc0467878636db1e083fb0d06ac5a75bac669
Stefan Buzu <stefanbuzu@gmail.com>
2024-05-10 13:36:42 +0200

    Added Efcdao and the endpoint

---
+++ weatherstation/Application/EfcDao/WeatherDao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using weatherstation.Application.DaoInterfaces;
+using 
+
+
+namespace weatherstation.Application.Dao
+{
+    internal class WeatherDao : IWeatherDao
+    {
+       private 
+        public Task<IEnumerable<WeatherData>> GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
