e95db97286fab515d6896e211f3d26a24f01b333
Cristian Josan <josan.c2003@gmail.com>
2024-05-14 18:05:49 +0200

    Update to db users table.

--- WEBSockets/Domain/Models/User.cs
+++ WEBSockets/Domain/Models/User.cs
@@ -6,15 +6,17 @@ namespace WEBSockets.Domain.Models
         public string Username { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
+        public string Preferences { get; set; }
         public bool OnNotifications { get; set; }
 
         public User() { }
 
-        public User(string username, string password, string email, bool onNotifications)
+        public User(string username, string password, string email, string preferences, bool onNotifications)
         {
             Username = username;
             Password = password;
             Email = email;
+            Preferences = preferences;
             OnNotifications = onNotifications;
         }
     }
---
+++ WEBSockets/Migrations/20240514160202_Add_Column_User_Preferences.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace WEBSockets.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Column_User_Preferences : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Username = table.Column<string>(type: "longtext", nullable: false),
+                    Password = table.Column<string>(type: "longtext", nullable: false),
+                    Email = table.Column<string>(type: "longtext", nullable: false),
+                    Preferences = table.Column<string>(type: "longtext", nullable: false),
+                    OnNotifications = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "WeatherData",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    WeatherState = table.Column<string>(type: "longtext", nullable: false),
+                    Temperature = table.Column<double>(type: "double", nullable: false),
+                    Light = table.Column<string>(type: "longtext", nullable: false),
+                    Humidity = table.Column<double>(type: "double", nullable: false),
+                    DateTime = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeatherData", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "WeatherData");
+        }
+    }
+}
