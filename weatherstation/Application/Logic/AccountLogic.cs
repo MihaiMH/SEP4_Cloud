983fc6727bccee343c4f8e0f2d045e902bcc7011
Stefan Buzu <stefanbuzu@gmail.com>
2024-05-14 15:27:13 +0200

    Added the Register account feature

---
+++ weatherstation/Application/Logic/AccountLogic.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using weatherstation.Application.LogicInterfaces;
+using weatherstation.Domain.DTOs;
+using weatherstation.Utils;
+
+namespace weatherstation.Application.Logic
+{
+    internal class AccountLogic : IAccountService
+    {
+        private readonly string connectionString; 
+
+        public AccountLogic(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public Task RegisterAccount(User user)
+        {
+            string query = $"INSERT INTO Users (Username, Password, Email, OnNotifications) " +
+                           $"VALUES ('Johnson', '123', 'johnson@gmail.com', 'true')";
+
+            try
+            {
+              
+                DBManager db = new DBManager(connectionString);
+
+                // Execute the command to insert the user into the database
+                // Since we're executing an INSERT query, we'll use ExecuteQuery with a dummy mapper
+                // Dummy mapper returns null because we don't need to map anything when inserting
+                // We only need to know if the insertion was successful or not
+                var results = db.ExecuteQuery(query, reader => (object)null);
+
+                // If the execution was successful (no exceptions), return a completed task
+                return Task.CompletedTask;
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"An error occurred during account registration: {ex.Message}");
+                throw; 
+            }
+        }
+    }
+}
---
+++ weatherstation/Application/LogicInterfaces/IAccountService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weatherstation.Application.LogicInterfaces
+{
+    internal interface IAccountService
+    {
+        Task RegisterAccount(User user);
+    }
+}
