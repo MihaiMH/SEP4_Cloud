39c2bf675f2ce77e23cc00d916513473107e760d
Mihai Mihaila <crut173@gmail.com>
2024-05-14 19:34:19 +0100

    Added new endpoint GetRecommendation which gets the client AI generated recommendations based on data from the IoT device

---
+++ weatherstation/Domain/Model/Preference.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weatherstation.Domain.Model
+{
+    internal class Preference
+    {
+        public string Text { get; set; }
+    }
+}
