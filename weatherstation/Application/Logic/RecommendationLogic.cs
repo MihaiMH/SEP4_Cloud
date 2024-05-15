2a8ec34732e9ae3523da770a01cc7aaba6f6abc1
Mihai Mihaila <crut173@gmail.com>
2024-05-15 01:20:41 +0100

    Fixes to GetRecommendation logic

--- weatherstation/Application/Logic/RecommendationLogic.cs
+++ weatherstation/Application/Logic/RecommendationLogic.cs
@@ -1,31 +1,22 @@
 ﻿using Newtonsoft.Json;
-using Org.BouncyCastle.Ocsp;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
 using weatherstation.Domain.DTOs;
 using weatherstation.Domain.Model;
 using weatherstation.Logic;
 using weatherstation.Utils;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace weatherstation.Application.Logic
 {
     internal static class RecommendationLogic
     {
-        private static readonly HttpClient httpClient = new HttpClient();
         public async static Task<string> GetRecommendation(dynamic data, Dictionary<string, string> token)
         {
             DBManager db = new DBManager(Environment.GetEnvironmentVariable("SQLCON1", EnvironmentVariableTarget.Process));
 
-            
-
             List<CurrentWeatherDto> weather = await WeatherLogic.GetCurrentWeather();
-
-            
 
             if (weather.Count == 0)
             {
@@ -35,7 +26,7 @@ namespace weatherstation.Application.Logic
 
             string pref = "No preferences";
 
-            if (token!=null)
+            if (token != null)
             {
                 string sqlCon = Environment.GetEnvironmentVariable("SQLCON1Q4", EnvironmentVariableTarget.Process);
 
@@ -45,9 +36,7 @@ namespace weatherstation.Application.Logic
                    async (reader) => new Preference
                    {
                        Text = reader.GetString("Preferences")
-
                    });
-                
 
                 if (preference.Count > 0 && !String.IsNullOrEmpty(preference[0].Text))
                 {
@@ -55,37 +44,41 @@ namespace weatherstation.Application.Logic
                 }
             }
 
-            return pref;
             string apiUrl = Environment.GetEnvironmentVariable("apiUrl", EnvironmentVariableTarget.Process);
             string apiKey = Environment.GetEnvironmentVariable("AIKEY", EnvironmentVariableTarget.Process);
-            httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
-            string query = Environment.GetEnvironmentVariable("AIQUERY", EnvironmentVariableTarget.Process); 
-            query = query.Replace("[WeatherState]", currentWeather.WeatherState.ToString()).Replace("[Temperature]", currentWeather.Temperature.ToString()).Replace("[Light]",currentWeather.Light).Replace("[Humidity]", currentWeather.Humidity.ToString()).Replace("[PREFERENCES]", pref);
 
-            var requestData = new
-
+            using (HttpClient httpClient = new HttpClient())
             {
-                model = "gpt-3.5-turbo",  // Set the model to gpt-3.5-turbo
-                messages = new[]
+                httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+                string query = Environment.GetEnvironmentVariable("AIQUERY", EnvironmentVariableTarget.Process);
+                query = query.Replace("[WeatherState]", currentWeather.WeatherState.ToString())
+                             .Replace("[Temperature]", currentWeather.Temperature.ToString())
+                             .Replace("[Light]", currentWeather.Light)
+                             .Replace("[Humidity]", currentWeather.Humidity.ToString())
+                             .Replace("[PREFERENCES]", pref);
+
+                var requestData = new
                 {
-                    new { role = "user", content = query }
-                },
-                max_tokens = 75  // Set the maximum number of tokens to generate
-            };
-            var content = new StringContent(JsonConvert.SerializeObject(requestData), System.Text.Encoding.UTF8, "application/json");
-            HttpResponseMessage response = await httpClient.PostAsync(apiUrl, content);
+                    model = "gpt-3.5-turbo",  // Set the model to gpt-3.5-turbo
+                    messages = new[]
+                    {
+                        new { role = "user", content = query }
+                    },
+                    max_tokens = 75  // Set the maximum number of tokens to generate
+                };
+                var content = new StringContent(JsonConvert.SerializeObject(requestData), System.Text.Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await httpClient.PostAsync(apiUrl, content);
 
-            
+                string result = await response.Content.ReadAsStringAsync();
+                dynamic obj = JsonConvert.DeserializeObject<dynamic>(result);
+                string msg = obj?.choices[0]?.message?.content;
+                if (msg == null)
+                {
+                    msg = "An error occurred and the recommendation could not be generated";
+                }
 
-            string result = await response.Content.ReadAsStringAsync();
-            dynamic obj = JsonConvert.DeserializeObject<dynamic>(result);
-            string msg = obj?.choices[0]?.message?.content;
-            if (msg == null)
-            {
-                msg = "An error occurred and the recommendation could not be generated";
+                return msg;
             }
-
-            return msg;
         }
     }
 }
